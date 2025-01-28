f96d518517f9c3f3727c8a0231e4a76c10caebc5
AnthonyMad-Mur <anthony.madrigal-mur@campus.mccd.edu>
2025-01-28 23:15:41 +0000

    Co-authored-by: AbrahamGonzalez05 <AbrahamGonzalez05@users.noreply.github.com> Co-authored-by: Cris-12345 <Cris-12345@users.noreply.github.com>

---
+++ Card.java
@@ -0,0 +1,68 @@
+public class Card{
+    
+    //Data Variables
+    private String suit;
+    private String rank;
+    private int value;
+    private String picture;
+
+    //default constructor
+    public Card(){
+        suit = "heart";
+        rank = "One";
+        value = 1;
+        picture = "pic.gif";
+    }
+
+    //initialized constructor
+    public Card(String suit, String rank, int value, String picture){
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+        this.picture = picture;
+    }
+
+    //Defining Equals
+    public boolean equal(Card other){
+        return rank.equals(other.rank);
+    }
+
+    //Setters
+    public void setSuit(String suit){
+        this.suit = suit;
+    }
+
+    public void setRank(String rank){
+        this.rank = rank;
+    }
+
+    public void setValue(int value){
+        this.value = value;
+    }
+
+    public void setPicture(String picture){
+        this.picture = picture;
+    }
+
+    //Getters
+    public String getSuit(){
+        return suit;
+    }
+
+    public String getRank(){
+        return rank;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public String getPicture(){
+        return picture;
+    }
+
+    //toString - Returns all varaibles
+    public String toString(){
+        return "suit = " + suit + "; rank = " + rank + "; value = " + value + "; picture = " + picture + ";";
+    }
+}
